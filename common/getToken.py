f06a2e3b142843e48a639c9625f9676466bca9fc
gaoxiaoyan <2268286535@qq.com>
2021-09-09 17:51:08 +0800

    add requirements.txt

--- common/getToken.py
+++ common/getToken.py
@@ -1,69 +1,53 @@
-import  unittest
-from  common.request import  Request
-import  json
+import unittest
+from common.request import Request
+import json
 from api_page.api_login import ApiLogin
-from common.read_json import   ReadJson
-from parameterized import parameterized  #作参数化 比ddt更加直观的一种方法
+from common.read_json import ReadJson
+from parameterized import parameterized  # 作参数化 比ddt更加直观的一种方法
 import os
 from common.getData import getData
 
 
-
-
-
 class GetToken:
-    def __init__(self,accountEmail,password,domain,port,protocol,base_url):
-        self.accountEmail=accountEmail
-        self.password=password
-        self.domain=domain
-        self.port = port
-        self.protocol=protocol
-        self.base_url=base_url
 
-
-    def getToken(self):
+    def getToken(self, protocol, domain, port, apiVersion, accountEmail, password, base_url, params):
         """
         获取登录后的token。
         :return:
         """
-        res=ApiLogin(self.protocol,self.domain,self.port).login_post(self.accountEmail,self.password,self.base_url)
-        # print(res.status_code)
-        # print(res.text)
-        # print(res.request.url)
-        # return res
+        res = ApiLogin(protocol, domain, port, apiVersion).login_post(accountEmail, password, base_url, params)
+
         result_json = res.json()
-        # print(result_json)
+        token_ = result_json["result"]["token"]  # dict type.
+        token = "Bearer " + token_
+        return token
+
+    def getToken_simple(self, responseObject):
+        """
+        通过响应对象直接获取token。
+        :return:
+        """
+        result_json = responseObject.json()
         token_ = result_json["result"]["token"]  # dict type.
         token = "Bearer " + token_
         return token
 
 
-
 if __name__ == '__main__':
-    top_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    configFile = os.path.join(top_dir, "config", "config.json")
-    # print("configFile",configFile)
-    data = getData(configFile, "v11")
-    #print(data)
+    protocol = 'http'
+    domain = 'rpa-test.datagrand.com'
+    port = 80
+    apiVersion = 'v2'
+    accountEmail = 'gaoxiaoyan@datagrand.com'
+    password = 'b29a8e35a7eeb51fd42c6abfb93597d9'
 
-    #获取配置文件内的参数
-    protocol = data["protocol"]
-    domain = data["domain"]
-    port = data["port"]
-    accountEmail = data["accountEmail"]
-    password = data["password"]
-    tenant = data["tenant"]
-    base_url_login=data["base_url_login"]
+    base_url = "token"
+    params = {"_allow_anonymous": "true", "selfHandled": "yes"}
 
+    token1 = GetToken().getToken(protocol, domain, port, apiVersion, accountEmail, password, base_url, params)
+    print('token1:',token1)
 
-    token= GetToken(accountEmail,password,domain,port,protocol,base_url_login).getToken()
-    print(token)
-
-
-
-
-
-
-
-
+    response = ApiLogin(protocol, domain, port, apiVersion).login_post(accountEmail, password, base_url, params)
+    token2=GetToken().getToken_simple(response)
+    print('token2:',token2)
 
